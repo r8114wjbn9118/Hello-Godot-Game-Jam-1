2909ff06ba7e2ca03eab7af8b7ccc32a63d5b884
r8114wjbn9118 <r8114wjbn9118@gmail.com>
2024-12-17 08:25:59 +0800

    update title scene

---
+++ script/title.gd
@@ -0,0 +1,26 @@
+extends Control
+class_name Title
+
+@onready var anim_tree = $AnimationTree
+
+#var select_level_scene = preload("res://scene/selectLevel.tscn")
+var select_level_scene = preload("res://Map/MapTest.tscn")
+
+func _ready() -> void:
+	anim_tree["parameters/conditions/start"] = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		anim_tree["parameters/conditions/start"] = true
+
+
+func enter_select_level_scene():
+	get_tree().change_scene_to_packed(select_level_scene)
+
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	print(anim_name)
+	if anim_name == "fadein":
+		enter_select_level_scene()
+	pass # Replace with function body.
