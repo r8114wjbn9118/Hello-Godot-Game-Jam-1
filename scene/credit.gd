cdd981eadd90b62bde53aeaea0237e0042ccd154
DennisFan <dennis515jd@gmail.com>
2024-12-22 07:35:15 +0800

    add to title

--- scene/credit.gd
+++ scene/credit.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_title_button_button_down() -> void:
+	GameManager.goto_scene("title")
