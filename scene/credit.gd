2c03ce7dbbfd5e7de18d737ed598b73c1ea33585
DennisFan <dennis515jd@gmail.com>
2024-12-22 09:51:28 +0800

    fix the end to play full ED

--- scene/credit.gd
+++ scene/credit.gd
@@ -1,7 +1,8 @@
 extends Control
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	GameManager.goto_scene("title")
+	pass
+	#GameManager.goto_scene("title")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
